1b3e112fee10428731cb082719a2d53891165687
hbarrons <hbarrons@gmail.com>
2022-01-31 17:39:14 -0500

    game is functional

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const game = {
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+      //console.log secretNum to help troubleshoot
       console.log(this.secretNum)
       // while loop to add player guess to prevGuess
        while (this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum) {
          this.prevGuesses.push(this.getGuess())
-         console.log(this.prevGuesses)};
-      this.render();
+         console.log(this.prevGuesses)
+         this.render()};
+      ;
   },
   // getGuess - do/while loop to provide player a prompt to submit guess until they guess correctly
   getGuess: function (){
@@ -29,15 +31,15 @@ const game = {
   },
   // function to alert depending on outcome of the guess
   render: function () {
-    let guessOutcome = null
-    if (this.prevGuesses[this.prevGuesses.length - 1] === this.secretNum) {
-      guessOutcome = `Congratulations! ${this.secretNum} was the correct number! It took you ${this.prevGuesses.length} tries to pick the correct number.`
-    } else if (this.prevGuesses[this.prevGuesses.length - 1] < this.secretNum) {
-      guessOutcome = `Too low, guess again! The number is between ${this.prevGuesses[this.prevGuesses.length - 1] } and ${this.biggestNum}`
-    } else {
-      guessOutcome = `Too high, guess again! The number is between ${this.biggestNum} and ${this.prevGuesses[this.prevGuesses.length - 1] }.`
-    }
-    alert(guessOutcome) 
+      if (this.prevGuesses[this.prevGuesses.length - 1] === this.secretNum) {
+        alert(`Congratulations, ${this.secretNum} was the correct number! It took you ${this.prevGuesses.length} tries to pick the correct number.`)
+      }
+      if (this.prevGuesses[this.prevGuesses.length - 1] < this.secretNum) {
+        alert(`Too low, guess again! You're previous guesses are ${this.prevGuesses}.`)
+      } 
+     if (this.prevGuesses[this.prevGuesses.length - 1] > this.secretNum) {
+        alert(`Too high, guess again! You're previous guesses are ${this.prevGuesses}.`)
+      }
   },
   
 }
@@ -45,5 +47,5 @@ const game = {
 
 game.play()
 
-//console.log for prevGuesses not working. Line 14.
+
 
