436ea1112659f04d7791aff29380422a70ccb3e7
hbarrons <hbarrons@gmail.com>
2022-01-30 21:24:35 -0500

    add render function

--- app.js
+++ app.js
@@ -14,8 +14,18 @@ const game = {
   prevGuesses: [],
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+      // do/while loop to add player guess to prevGuess
+      do {
+        this.prevGuesses.unshift(this.getGuess)
+        this.render()
+      } while (
+        this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum
+      );
+      // invoke getGuess
+      getGuess();
   },
+  // getGuess - do/while loop to provide player a prompt to submit guess until they guess correctly
   getGuess: function (){
     let guess = null
     // the do will give the player the prompt
@@ -29,21 +39,23 @@ const game = {
       guess === NaN,
       guess !== this.secretNum
       );
+      return guess
   },
-  //function to alert depending on outcome of the guess
-  // render: function (guess) {
-  //   if (guess === secretNum) {
-  //     return `Congratulations! ${this.secretNum} was the correct number! It took you ${this.prevGuesses.length} tries to pick the correct number.`
-  //   } else if (guess < this.secretNum) {
-  //     return `Too low, guess again! The number is between ${this.prevGuesses[0]} and ${this.biggestNum}`
-  //   } else {
-  //     return `Too high, guess again! The number is between ${this.biggestNum} and ${this.prevGuesses[0]}.`
-  //   }
-  // }
+  // function to alert depending on outcome of the guess
+  render: function () {
+    let guessOutcome
+    if (this.prevGuesses[this.prevGuesses.length - 1] === secretNum) {
+      guessOutcome = `Congratulations! ${this.secretNum} was the correct number! It took you ${this.prevGuesses.length} tries to pick the correct number.`
+    } else if (this.prevGuesses[this.prevGuesses.length - 1] < this.secretNum) {
+      guessOutcome = `Too low, guess again! The number is between ${this.prevGuesses[0]} and ${this.biggestNum}`
+    } else {
+      guessOutcome = `Too high, guess again! The number is between ${this.biggestNum} and ${this.prevGuesses[0]}.`
+    }
+  },
   
 }
-console.log("test")
-game.play();
+
+game.play()
 
 
 
